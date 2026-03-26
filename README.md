1c44d1f2500b225b82d0bad7119c8bf68c7790a1
AnastasiaYakushina <aubarr@ya.ru>
2026-03-26 14:15:18 +0300

    Add templates

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/AnastasiaYakushina/php-project-9/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/AnastasiaYakushina/php-project-9/actions)
+
+https://php-project-9-8xoz.onrender.com
--- public/index.php
+++ public/index.php
@@ -6,11 +6,20 @@ use Slim\Factory\AppFactory;
 use DI\Container;
 
 $container = new Container();
+
+$container->set('renderer', function () {
+    $renderer = new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+    $renderer->setLayout('layout.phtml');
+    return $renderer;
+});
+
 $app = AppFactory::createFromContainer($container);
 
 $app->get('/', function ($request, $response) {
-    $response->getBody()->write("Hello, Slim!");
-    return $response;
+    $params = [
+        'flash' => [],
+    ];
+    return $this->get('renderer')->render($response, 'index.phtml', $params);
 });
 
 $app->run();
