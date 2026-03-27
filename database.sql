c638663db717b78c5943b4c03af2435654a9216c
AnastasiaYakushina <aubarr@ya.ru>
2026-03-28 00:23:44 +0300

    Add URL recording and display pages

--- database.sql
+++ database.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS public.urls (
-	id serial4 NOT NULL,
-	"name" varchar(255) NOT NULL UNIQUE,
-	created_at timestamp DEFAULT CURRENT_TIMESTAMP NULL,
-	CONSTRAINT urls_pkey PRIMARY KEY (id)
+    id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name varchar(255) NOT NULL UNIQUE,
+    created_at timestamptz NOT NULL
 );
+
--- public/index.php
+++ public/index.php
@@ -2,6 +2,8 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+session_start();
+
 use Slim\Factory\AppFactory;
 use DI\Container;
 
@@ -25,29 +27,128 @@ $container->set('renderer', function () {
 $container->set(\PDO::class, function () {
     $databaseUrl = getenv('DATABASE_URL');
 
-    $databaseUrl = parse_url($databaseUrl);
+    $databaseUrlParams = parse_url($databaseUrl);
 
     $dsn = sprintf(
         "pgsql:host=%s;port=%d;dbname=%s",
-        $databaseUrl['host'],
-        $databaseUrl['port'] ?? 5432,
-        ltrim($databaseUrl['path'], '/')
+        $databaseUrlParams['host'],
+        $databaseUrlParams['port'] ?? 5432,
+        ltrim($databaseUrlParams['path'], '/')
     );
 
-    $conn = new \PDO($dsn, $databaseUrl['user'], $databaseUrl['pass']);
+    $conn = new \PDO($dsn, $databaseUrlParams['user'], $databaseUrlParams['pass']);
     $conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
     $conn->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
 
     return $conn;
 });
 
+$container->set('flash', function () {
+    return new Slim\Flash\Messages();
+});
+
 $app = AppFactory::createFromContainer($container);
 
+$router = $app->getRouteCollector()->getRouteParser();
+
 $app->get('/', function ($request, $response) {
+    $flash = $this->get('flash')->getMessages();
+
     $params = [
-        'flash' => [],
+        'url' => '',
+        'errors' => [],
+        'flash' => $flash,
     ];
     return $this->get('renderer')->render($response, 'index.phtml', $params);
+})->setName('home');
+
+$app->get('/urls/{id}', function ($request, $response, $args) {
+    $flash = $this->get('flash')->getMessages();
+
+    $id = $args['id'];
+
+    $pdo = $this->get(\PDO::class);
+    $sql = "SELECT * FROM urls WHERE id = :id";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':id', $id);
+    $stmt->execute();
+    $url = $stmt->fetch();
+
+    $params = [
+        'url' => $url,
+        'errors' => [],
+        'flash' => $flash,
+    ];
+    return $this->get('renderer')->render($response, 'urls/show.phtml', $params);
+})->setName('url');
+
+$app->get('/urls', function ($request, $response) {
+    $flash = $this->get('flash')->getMessages();
+
+    $pdo = $this->get(\PDO::class);
+    $sql = "SELECT * FROM urls ORDER BY created_at DESC";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+    $urls = $stmt->fetchAll();
+
+    $params = [
+        'urls' => $urls,
+        'errors' => [],
+        'flash' => $flash,
+    ];
+    return $this->get('renderer')->render($response, 'urls/index.phtml', $params);
+})->setName('urls');
+
+$app->post('/urls', function ($request, $response) use ($router) {
+
+    $data = $request->getParsedBody();
+    $url = $data['url'] ?? '';
+
+    $parsedUrl = parse_url(strtolower(trim($url)));
+    
+    $scheme = isset($parsedUrl['scheme']) ? strtolower($parsedUrl['scheme']) : 'http';
+    $host = isset($parsedUrl['host']) ? strtolower($parsedUrl['host']) : '';
+
+    if ($host) {
+        $normalizedUrl = "{$scheme}://{$host}";
+    } else {
+        $normalizedUrl = $url;
+    }
+
+    $validator = new \Valitron\Validator(['url' => $normalizedUrl]);
+  
+    $validator->rule('required', 'url')->message('URL не должен быть пустым');
+    $validator->rule('url', 'url')->message('Некорректный URL');
+    $validator->rule('lengthMax', 'url', 255)->message('URL превышает 255 символов');
+
+    if ($validator->validate()) {
+        try {
+            $pdo = $this->get(\PDO::class);
+            $createdAt = \Carbon\Carbon::now()->toDateTimeString();
+            $sql = "INSERT INTO urls (name, created_at) VALUES (:name, :created_at)";
+            $stmt = $pdo->prepare($sql);
+            $stmt->bindParam(':name', $normalizedUrl);
+            $stmt->bindParam(':created_at', $createdAt);
+            $stmt->execute();
+
+            $id = $pdo->lastInsertId();
+            
+            $this->get('flash')->addMessage('success', "Страница успешно добавлена");
+            return $response->withRedirect($router->urlFor('url', ['id' => $id]), 303);
+
+        } catch (\PDOException $e) {
+            if ($e->getCode() === '23505') {
+                $this->get('flash')->addMessage('danger', "Страница уже существует");
+                return $response->withRedirect($router->urlFor('home'), 303);
+            }
+        }
+    }
+
+    $params = ['url' => $url,
+               'errors' => $validator->errors(),
+               'flash' => $this->get('flash')->getMessages()
+    ];
+    return $this->get('renderer')->render($response->withStatus(422), 'index.phtml', $params);
 });
 
 $app->run();
